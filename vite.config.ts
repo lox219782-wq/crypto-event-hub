4c210b6f3526dd7f866192d0d425d713dae3db62
lox219782-wq <lox219782@gmail.com>
2026-05-17 01:27:34 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,4 +12,10 @@ export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
+  vite: {
+    base: '/crypto-event-hub/',
+    build: {
+      outDir: 'dist',
+    }
+  }
 });
